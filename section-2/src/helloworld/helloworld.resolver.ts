3f03fdeab870019eedeeb0cf71699a59c119c70f
Dayan Perez <dayan.perez9012@gmail.com>
2023-01-11 20:07:57 -0300

    add more queries

--- section-2/src/helloworld/helloworld.resolver.ts
+++ section-2/src/helloworld/helloworld.resolver.ts
@@ -1,9 +1,26 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { ParseIntPipe } from '@nestjs/common';
+import { Resolver, Query, Float, Int, Args } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloworldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: 'hello',
+    description: 'Hola mundo es lo que retorna',
+  })
   helloWorld(): string {
     return 'Hello World';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomNumberFromZero',
+    description: 'From zero to argument to',
+  })
+  getRandomNumberFromZero(@Args('to', { type: () => Int }) to: number) {
+    return Math.floor(Math.random() * to);
   }
 }
